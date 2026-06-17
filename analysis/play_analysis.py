2bdb361a65603ce3050bc1d4f22ee48de0e4c9d6
marina <marina.mendietarr@gmail.com>
2026-06-17 23:55:36 +0200

    Update play_analysis.py

--- analysis/play_analysis.py
+++ analysis/play_analysis.py
@@ -1,6 +1,7 @@
 """
 Play-level situational analysis engine.
 Ingests a tagged play-by-play Excel/CSV and answers:
+  - What does a team do on a given down & distance?
   - What formations/personnel do they favour in each situation?
   - What are their run tendencies by direction and field zone?
   - What are their pass tendencies by depth and coverage situation?
